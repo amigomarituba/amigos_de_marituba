1ccd3ae71bd64f9914d90f72f390277199288039
IgorCosta28 <76978556+IgorCosta28@users.noreply.github.com>
2026-03-18 22:33:57 -0300

    EDITER leader.id

--- src/views/regencia/Cidadoes.js
+++ src/views/regencia/Cidadoes.js
@@ -612,7 +612,7 @@ const Cidadoes = () => {
                   style={{ marginBottom: 3 }}
                   floatingLabel="Lider Responsáve"
                   aria-label="Floating label select example"
-                  {...register('leader_id')}
+                  {...register('leader.id')}
                 >
                   <option value={''}>Sem Lider</option>
                   {leaders.map((leader) => {
