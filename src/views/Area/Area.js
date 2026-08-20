5c659fd2b486b950899bf5c8ec9bd319f99bb8c6
IgorCosta28 <76978556+IgorCosta28@users.noreply.github.com>
2026-08-20 17:56:27 -0300

    bug: correcao do bug do mapa

--- src/views/Area/Area.js
+++ src/views/Area/Area.js
@@ -38,6 +38,20 @@ import { Card } from 'rsuite'
 import ContentCopyIcon from '@mui/icons-material/ContentCopy'
 import AddIcon from '@mui/icons-material/Add'
 
+import L from 'leaflet'
+
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
+import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png'
+
+delete L.Icon.Default.prototype._getIconUrl
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+})
+
 import MapIcon from '@mui/icons-material/Map'
 import 'leaflet/dist/leaflet.css'
 const localizacoes = [
